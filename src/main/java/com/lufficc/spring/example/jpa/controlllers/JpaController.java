91dbc7b781c59d0ad16ae4369b36593da3128906
lufficc <luffy.lcc@gmail.com>
2017-03-27 21:28:29 +0800

    add posts

--- src/main/java/com/lufficc/spring/example/jpa/controlllers/JpaController.java
+++ src/main/java/com/lufficc/spring/example/jpa/controlllers/JpaController.java
@@ -1,5 +1,6 @@
 package com.lufficc.spring.example.jpa.controlllers;
 
+import com.lufficc.spring.example.jpa.models.Post;
 import com.lufficc.spring.example.jpa.models.QPost;
 import com.lufficc.spring.example.jpa.models.QUser;
 import com.lufficc.spring.example.jpa.models.User;
@@ -7,12 +8,12 @@ import com.lufficc.spring.example.jpa.repositories.PostRepository;
 import com.lufficc.spring.example.jpa.repositories.UserRepository;
 import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.ComparableExpressionBase;
-import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.querydsl.binding.QuerydslPredicate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,7 +23,6 @@ import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -33,153 +33,151 @@ import java.util.stream.Collectors;
  */
 @RestController
 public class JpaController {
-	private final Logger logger = Logger.getLogger(JpaController.class);
+    private final Logger logger = Logger.getLogger(JpaController.class);
 
-	@PersistenceContext
-	private EntityManager entityManager;
+    @PersistenceContext
+    private EntityManager entityManager;
 
-	private final UserRepository userRepository;
-	private final PostRepository postRepository;
-	private JPAQueryFactory queryFactory;
+    private final UserRepository userRepository;
+    private final PostRepository postRepository;
+    private JPAQueryFactory queryFactory;
 
-	@Autowired
-	public JpaController(UserRepository userRepository, PostRepository postRepository) {
-		this.userRepository = userRepository;
-		this.postRepository = postRepository;
-	}
+    @Autowired
+    public JpaController(UserRepository userRepository, PostRepository postRepository) {
+        this.userRepository = userRepository;
+        this.postRepository = postRepository;
+    }
 
-	@PostConstruct
-	public void init() {
-		queryFactory = new JPAQueryFactory(entityManager);
-	}
+    @PostConstruct
+    public void init() {
+        queryFactory = new JPAQueryFactory(entityManager);
+    }
 
-	@GetMapping("/users/emails")
-	public Object userEmails() {
-		QUser user = QUser.user;
-		userRepository.findAll(user.name.eq("lufifcc"));
-		userRepository.findAll(
-				user.email.endsWith("@gmail.com")
-						.and(user.name.startsWith("lu"))
-						.and(user.id.in(Arrays.asList(520L, 1314L, 1L, 2L, 12L)))
-		);
+    @GetMapping("/users/emails")
+    public Object userEmails() {
+        QUser user = QUser.user;
+        userRepository.findAll(user.name.eq("lufifcc"));
+        userRepository.findAll(
+                user.email.endsWith("@gmail.com")
+                        .and(user.name.startsWith("lu"))
+                        .and(user.id.in(Arrays.asList(520L, 1314L, 1L, 2L, 12L)))
+        );
 
-		userRepository.count(
-				user.email.endsWith("@outlook.com")
-						.and(user.name.containsIgnoreCase("a"))
-		);
+        userRepository.count(
+                user.email.endsWith("@outlook.com")
+                        .and(user.name.containsIgnoreCase("a"))
+        );
 
-		userRepository.findAll(
-				user.email.endsWith("@outlook.com")
-						.and(user.posts.size().goe(5))
-		);
+        userRepository.findAll(
+                user.email.endsWith("@outlook.com")
+                        .and(user.posts.size().goe(5))
+        );
 
-		userRepository.findAll(
-				user.posts.size().goe(JPAExpressions.select(user.posts.size().max()).from(user))
-		);
+        userRepository.findAll(
+                user.posts.size().goe(JPAExpressions.select(user.posts.size().max()).from(user))
+        );
 
-		return queryFactory.selectFrom(user)
-				.select(user.email)
-				.fetch();
-	}
+        return queryFactory.selectFrom(user)
+                .select(user.email)
+                .fetch();
+    }
 
-	@GetMapping("users/names-emails")
-	public Object userNamesEmails() {
-		QUser user = QUser.user;
-		return queryFactory.selectFrom(user)
-				.select(user.email, user.name)
-				.fetch()
-				.stream()
-				.map(tuple -> {
-					Map<String, String> map = new LinkedHashMap<>();
-					map.put("name", tuple.get(user.name));
-					map.put("email", tuple.get(user.email));
-					return map;
-				}).collect(Collectors.toList());
-	}
+    @GetMapping("users/names-emails")
+    public Object userNamesEmails() {
+        QUser user = QUser.user;
+        return queryFactory.selectFrom(user)
+                .select(user.email, user.name)
+                .fetch()
+                .stream()
+                .map(tuple -> {
+                    Map<String, String> map = new LinkedHashMap<>();
+                    map.put("name", tuple.get(user.name));
+                    map.put("email", tuple.get(user.email));
+                    return map;
+                }).collect(Collectors.toList());
+    }
 
 
-	@GetMapping("users")
-	public Object users(@QuerydslPredicate(root = User.class) Predicate predicate) {
-		return userRepository.findAll(predicate);
-	}
+    @GetMapping("users")
+    public Object users(@QuerydslPredicate(root = User.class) Predicate predicate, Pageable pageable) {
+        return userRepository.findAll(predicate, pageable);
+    }
 
-	@GetMapping("users/{user}/posts")
-	public Object posts(@PathVariable User user) {
-		return user.getPosts();
-	}
+    @GetMapping("users/{user}/posts")
+    public Object posts(@PathVariable User user) {
+        return user.getPosts();
+    }
 
-	@GetMapping("users/posts-count")
-	public Object postCount() {
-		QUser user = QUser.user;
-		QPost post = QPost.post;
-		return queryFactory.selectFrom(user)
-				.leftJoin(user.posts, post)
-				.select(user.id, user.name, post.count())
-				.groupBy(user.id)
-				.fetch()
-				.stream()
-				.map(tuple -> {
-					Map<String, Object> map = new LinkedHashMap<>();
-					map.put("id", tuple.get(user.id));
-					map.put("name", tuple.get(user.name));
-					map.put("posts_count", tuple.get(post.count()));
-					return map;
-				}).collect(Collectors.toList());
-	}
+    @GetMapping("users/posts-count")
+    public Object postCount() {
+        QUser user = QUser.user;
+        QPost post = QPost.post;
+        return queryFactory.selectFrom(user)
+                .leftJoin(user.posts, post)
+                .select(user.id, user.name, post.count())
+                .groupBy(user.id)
+                .fetch()
+                .stream()
+                .map(tuple -> {
+                    Map<String, Object> map = new LinkedHashMap<>();
+                    map.put("id", tuple.get(user.id));
+                    map.put("name", tuple.get(user.name));
+                    map.put("posts_count", tuple.get(post.count()));
+                    return map;
+                }).collect(Collectors.toList());
+    }
 
-	@GetMapping("users/category-count")
-	public Object postCategoryMax() {
-		QUser user = QUser.user;
-		QPost post = QPost.post;
-		NumberExpression<Integer> java = post.category
-				.name.lower().when("java").then(1).otherwise(0);
-		NumberExpression<Integer> python = post.category
-				.name.lower().when("python").then(1).otherwise(0);
-		return queryFactory.selectFrom(user)
-				.leftJoin(user.posts, post)
-				.select(user.name, user.id, java.sum(), python.sum(), post.count())
-				.groupBy(user.id)
-				.orderBy(user.name.desc())
-				.fetch()
-				.stream()
-				.map(tuple -> {
-					Map<String, Object> map = new LinkedHashMap<>();
-					map.put("username", tuple.get(user.name));
-					map.put("java_count", tuple.get(java.sum()));
-					map.put("python_count", tuple.get(python.sum()));
-					map.put("total_count", tuple.get(post.count()));
-					return map;
-				}).collect(Collectors.toList());
-	}
+    @GetMapping("users/category-count")
+    public Object postCategoryMax() {
+        QUser user = QUser.user;
+        QPost post = QPost.post;
+        NumberExpression<Integer> java = post.category
+                .name.lower().when("java").then(1).otherwise(0);
+        NumberExpression<Integer> python = post.category
+                .name.lower().when("python").then(1).otherwise(0);
+        return queryFactory.selectFrom(user)
+                .leftJoin(user.posts, post)
+                .select(user.name, user.id, java.sum(), python.sum(), post.count())
+                .groupBy(user.id)
+                .orderBy(user.name.desc())
+                .fetch()
+                .stream()
+                .map(tuple -> {
+                    Map<String, Object> map = new LinkedHashMap<>();
+                    map.put("username", tuple.get(user.name));
+                    map.put("java_count", tuple.get(java.sum()));
+                    map.put("python_count", tuple.get(python.sum()));
+                    map.put("total_count", tuple.get(post.count()));
+                    return map;
+                }).collect(Collectors.toList());
+    }
 
-	@GetMapping("posts")
-	public Object posts() {
-		return postRepository.findAll();
-	}
+    @GetMapping("posts")
+    public Object posts(@QuerydslPredicate(root = Post.class) Predicate predicate) {
+        return postRepository.findAll(predicate);
+    }
 
-	@GetMapping("posts-summary")
-	public Object postsSummary() {
-		QPost post = QPost.post;
-		ComparableExpressionBase<?> postTimePeriodsExp =
-				Expressions.dateTemplate(Date.class, "convert(date, {0})", "yyyy-mm");
-
-		NumberExpression<Integer> java = post.category
-				.name.lower().when("java").then(1).otherwise(0);
-		NumberExpression<Integer> python = post.category
-				.name.lower().when("python").then(1).otherwise(0);
-		return queryFactory.selectFrom(post)
-				.groupBy(postTimePeriodsExp)
-				.select(postTimePeriodsExp, java.sum(), python.sum(), post.count())
-				.orderBy(postTimePeriodsExp.desc())
-				.fetch()
-				.stream()
-				.map(tuple -> {
-					Map<String, Object> map = new LinkedHashMap<>();
-					map.put("time_period", tuple.get(postTimePeriodsExp));
-					map.put("java_count", tuple.get(java.sum()));
-					map.put("python_count", tuple.get(python.sum()));
-					map.put("total_count", tuple.get(post.count()));
-					return map;
-				}).collect(Collectors.toList());
-	}
+    @GetMapping("posts-summary")
+    public Object postsSummary() {
+        QPost post = QPost.post;
+        ComparableExpressionBase<?> postTimePeriodsExp = post.createdAt.year();
+        NumberExpression<Integer> java = post.category
+                .name.lower().when("java").then(1).otherwise(0);
+        NumberExpression<Integer> python = post.category
+                .name.lower().when("python").then(1).otherwise(0);
+        return queryFactory.selectFrom(post)
+                .groupBy(postTimePeriodsExp)
+                .select(postTimePeriodsExp, java.sum(), python.sum(), post.count())
+                .orderBy(postTimePeriodsExp.asc())
+                .fetch()
+                .stream()
+                .map(tuple -> {
+                    Map<String, Object> map = new LinkedHashMap<>();
+                    map.put("time_period", tuple.get(postTimePeriodsExp));
+                    map.put("java_count", tuple.get(java.sum()));
+                    map.put("python_count", tuple.get(python.sum()));
+                    map.put("total_count", tuple.get(post.count()));
+                    return map;
+                }).collect(Collectors.toList());
+    }
 }
--- src/main/java/com/lufficc/spring/example/jpa/repositories/PostRepository.java
+++ src/main/java/com/lufficc/spring/example/jpa/repositories/PostRepository.java
@@ -1,7 +1,12 @@
 package com.lufficc.spring.example.jpa.repositories;
 
 import com.lufficc.spring.example.jpa.models.Post;
+import com.lufficc.spring.example.jpa.models.QPost;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -9,5 +14,8 @@ import org.springframework.stereotype.Repository;
  * When 2017/2/20
  */
 @Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long>, QueryDslPredicateExecutor<Post>, QuerydslBinderCustomizer<QPost> {
+    default void customize(QuerydslBindings bindings, QPost post) {
+        bindings.bind(post.title).first(StringExpression::containsIgnoreCase);
+    }
 }
